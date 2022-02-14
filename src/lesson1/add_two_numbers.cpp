173a31e24b7b6cf6e59d53d7d2010d6d6b338616
Ovie, Voushetectt Destiny <41767069+voushe@users.noreply.github.com>
2022-02-14 01:36:40 +0100

    Update add_two_numbers.cpp

--- src/lesson1/add_two_numbers.cpp
+++ src/lesson1/add_two_numbers.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-auto add = [](int first_number, int second_number, int sum = 0){//lambda expression
+auto add = [](int first_number, int second_number, int sum = 0){//lambda function
     return sum = first_number + second_number;
 };
 
@@ -11,7 +11,7 @@ int main(){
     std::cin>>num1;//request user input
     std::cout<<"Please enter the second number ::";
     std::cin>>num2;//request user input
-    auto sum = add(num1, num2);//initializes sum with the return value
+    auto sum = add(num1, num2);//initializes sum with the return value from the lambda add function
     std::cout<<"Sum = "<<sum<<std::endl;
     return(0);
 }
