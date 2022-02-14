caf08f1c4df3dfd43f845ee0c4811c6c80793a35
Ovie, Voushetectt Destiny <41767069+voushe@users.noreply.github.com>
2022-02-14 01:41:12 +0100

    Update maximum_minimum.cpp

--- src/lesson2/maximum_minimum.cpp
+++ src/lesson2/maximum_minimum.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-int maximum(int a, int b, int c, int max = 0){//a lambda function that returns the maximum value between a, b, c
+int maximum(int a, int b, int c, int max = 0){//a lambda expression that returns the maximum value between a, b, c
     return max = b > c && b > a ? b : c > b && c > a ? c : a;//uses conditional operator
 }
 
-int minimum(int a, int b, int c, int min = 0){//a lambda function that returns the minimum value between a, b, c
+int minimum(int a, int b, int c, int min = 0){//a lambda expression that returns the minimum value between a, b, c
     return min = b < c && b < a ? b : c < b && c < a ? c : a;//uses conditional operator
 }
 
@@ -20,8 +20,8 @@ int main(){
     std::cin>>second_number;//request user input
     std::cout<<"Please enter the third number :: ";
     std::cin>>third_number;
-    max = maximum(first_number, second_number, third_number);//calling the lambda function
-    min = minimum(first_number, second_number, third_number);//calling the lambda function
+    max = maximum(first_number, second_number, third_number);//calling the lambda expression
+    min = minimum(first_number, second_number, third_number);//calling the lambda expression
     std::cout<<"Maximum = "<<max<<std::endl;
     std::cout<<"Minimum = "<<min<<std::endl;
     return(0);
